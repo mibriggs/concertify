39e1bf1047ce30f38ef006e7f8ad5643e48a2919
mibriggs <33757644+mibriggs@users.noreply.github.com>
2024-06-13 02:11:25 -0400

    used bind:this

--- src/lib/components/search-bar.svelte
+++ src/lib/components/search-bar.svelte
@@ -1,23 +1,28 @@
 <script lang="ts">
 	import { X } from 'lucide-svelte';
-	import { fade, scale } from 'svelte/transition';
+	import { scale } from 'svelte/transition';
 
 	export let placeholder: string;
 	export let value: string;
+	let searchBarElement: HTMLElement;
 
 	const clearSearchBar = () => {
-		const searchElement: HTMLElement | null = document.getElementById('search');
-		if (searchElement) {
-			searchElement.focus();
-			value = '';
-		}
+		searchBarElement.focus();
+		value = '';
 	};
 </script>
 
 <div
 	class="flex w-full items-center justify-between rounded-xl bg-stone-300 bg-search bg-left-center bg-no-repeat py-2 pl-12 text-spotiblack outline-none"
 >
-	<input id="search" type="text" class="w-full bg-inherit outline-none" {placeholder} bind:value />
+	<input
+		id="search"
+		type="text"
+		class="w-full bg-inherit outline-none"
+		{placeholder}
+		bind:value
+		bind:this={searchBarElement}
+	/>
 	{#if value.length > 0}
 		<button
 			on:click={clearSearchBar}
