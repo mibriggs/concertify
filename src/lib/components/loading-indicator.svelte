6098f209b29b0cfad15c6a92b1dc4b8afff08926
mibriggs <33757644+mibriggs@users.noreply.github.com>
2026-01-09 22:41:15 -0500

    spinner on new line

--- src/lib/components/loading-indicator.svelte
+++ src/lib/components/loading-indicator.svelte
@@ -2,7 +2,7 @@
 	export let message: string = 'Loading more artists...';
 </script>
 
-<div class="flex w-full items-center justify-center gap-2 py-8 text-white">
+<div class="col-span-full flex w-full items-center justify-center gap-2 py-8 text-white">
 	<svg
 		class="h-6 w-6 animate-spin"
 		xmlns="http://www.w3.org/2000/svg"
